0d3d3372d9c246e6d64e359155668cdcf0a3c440
Erin Walker <erinb0317@hotmail.com>
2018-01-28 20:42:58 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,7 @@
-def using_include(array, element)
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 
+def using_include(array, element)
+  array.include?(element)
 end
 
 def using_sort(array)
